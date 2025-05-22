b7c788284d64a7f0a86050279a4e23cbca16e105
Byungjin Choi <chrisholic22@gmail.com>
2025-05-22 09:09:04 +0900

    🧱 feat: restore virtualized PixelGrid with modal trigger on pixel click

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { HeroSection } from "@/components/homepage/HeroSection";
-import { PixelCanvas } from "@/components/homepage/PixelCanvas";
+import { PixelGrid } from "@/components/homepage/PixelGrid";
 import { HowItWorks } from "@/components/homepage/HowItWorks";
 import { Footer } from "@/components/common/Footer";
 
@@ -8,7 +8,7 @@ export default function HomePage() {
     <div className="flex flex-col min-h-screen">
       <HeroSection />
       <main className="flex-1 px-4 md:px-12 lg:px-20">
-        <PixelCanvas />
+        <PixelGrid />
         <HowItWorks />
       </main>
       <Footer />
