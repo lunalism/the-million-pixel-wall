d64946aed97984523529ea02ba04782cc20dd426
Byungjin Choi <chrisholic22@gmail.com>
2025-05-26 09:22:12 +0900

    change some code

--- app/head.tsx
+++ app/head.tsx
@@ -1,23 +1,28 @@
 // app/head.tsx
 export default function Head() {
     return (
-      <>
-        <title>The Million Pixel Wall</title>
-        <meta name="title" content="The Million Pixel Wall" />
-        <meta name="description" content="Leave your brand in digital history." />
-  
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://the-million-pixel-wall.vercel.app/" />
-        <meta property="og:title" content="The Million Pixel Wall" />
-        <meta property="og:description" content="Leave your brand in digital history." />
-        <meta property="og:image" content="https://the-million-pixel-wall.vercel.app/og-home.jpg" />
-  
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://the-million-pixel-wall.vercel.app/" />
-        <meta property="twitter:title" content="The Million Pixel Wall" />
-        <meta property="twitter:description" content="Leave your brand in digital history." />
-        <meta property="twitter:image" content="https://the-million-pixel-wall.vercel.app/og-home.jpg" />
-      </>
+        <>
+            {/* <!-- Primary Meta Tags --> */}
+            <title>The Million Pixel Wall</title>
+            <meta name="title" content="The Million Pixel Wall" />
+            <meta name="description" content="Leave your brand in digital history." />
+
+            {/* <!-- Open Graph / Facebook --> */}
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://the-million-pixel-wall.vercel.app/" />
+            <meta property="og:title" content="The Million Pixel Wall" />
+            <meta property="og:description" content="Leave your brand in digital history." />
+            <meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+            {/* <!-- Twitter --> */}
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://the-million-pixel-wall.vercel.app/" />
+            <meta property="twitter:title" content="The Million Pixel Wall" />
+            <meta property="twitter:description" content="Leave your brand in digital history." />
+            <meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+            {/* <!-- Meta Tags Generated with https://metatags.io --> */}
+        </>
     );
   }
   
